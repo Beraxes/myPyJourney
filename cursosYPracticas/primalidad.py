deab327497bf0bcd93d5dcf9fea4b4e9066fba0e
Javier Andres Olivera Soto <javierandresoliverasoto@gmail.com>
2022-09-14 09:55:02 -0500

    primalidad.py terminada

--- cursosYPracticas/primalidad.py
+++ cursosYPracticas/primalidad.py
@@ -1,5 +1,15 @@
-def esPrimo():
-        pass
+def esPrimo(numero):
+        contador = 0
+        for i in range(1, numero+1):
+                if i == 1 or i == numero:
+                  continue
+                if numero % i==0:
+                        contador += 1
+        if contador == 0:
+                return True
+        else:
+          return False
+         
 
 def run():
         numero = int(input("Escribe un numero: "))
